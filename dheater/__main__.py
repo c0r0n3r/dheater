40df69316d640cc62034056b1da08af45686bb9c
Szilárd Pfeiffer <szilard.pfeiffer@balasys.hu>
2021-09-20 13:44:15 +0200

    feat!: Provide statistics at the end of the heating

--- dheater/__main__.py
+++ dheater/__main__.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import os
 import time
 import socket
 import threading
@@ -23,11 +24,23 @@ from cryptolyzer.tls.client import (
 
 
 @attr.s(eq=False)
+class DHEnforcerThreadStats(threading.Thread):
+    failed_request_num = attr.ib(init=False, default=0, validator=attr.validators.instance_of(int))
+    succeeded_request_num = attr.ib(init=False, default=0, validator=attr.validators.instance_of(int))
+    sent_byte_count = attr.ib(init=False, default=0, validator=attr.validators.instance_of(int))
+    received_byte_count = attr.ib(init=False, default=0, validator=attr.validators.instance_of(int))
+    time_interval = attr.ib(init=False, default=0, validator=attr.validators.instance_of(int))
+
+
+@attr.s(eq=False)
 class DHEnforcerThreadBase(threading.Thread):
     uri = attr.ib(validator=attr.validators.instance_of(urllib3.util.url.Url))
     timeout = attr.ib(validator=attr.validators.instance_of(int))
     message_bytes = attr.ib(init=False, default=bytearray(), validator=attr.validators.instance_of(bytearray))
     stop = attr.ib(init=False, default=False, validator=attr.validators.instance_of(bool))
+    stats = attr.ib(
+        init=False, default=DHEnforcerThreadStats(), validator=attr.validators.instance_of(DHEnforcerThreadStats)
+    )
 
     @abc.abstractmethod
     def _get_client(self):
@@ -47,13 +60,21 @@ class DHEnforcerThreadBase(threading.Thread):
         self.message_bytes = self._prepare_packets()
 
     def run(self):
+        start_time = time.time()
         while not self.stop:
             try:
                 client = self._get_client()
                 client.init_connection()
-                self._send_packets(client)
+                sent_byte_count, received_byte_count = self._send_packets(client)
             except (ConnectionResetError, socket.timeout, socket.error, InvalidType, SecurityError):
-                pass
+                self.stats.failed_request_num += 1
+            else:
+                self.stats.received_byte_count += received_byte_count
+                self.stats.sent_byte_count += sent_byte_count
+                self.stats.succeeded_request_num += 1
+        end_time = time.time()
+
+        self.stats.time_interval = end_time - start_time
 
 
 class DHEnforcerThreadTLS(DHEnforcerThreadBase):
@@ -73,7 +94,7 @@ class DHEnforcerThreadTLS(DHEnforcerThreadBase):
         return client_hello_bytes
 
     def _send_packets(self, client):
-        client.send(self.message_bytes)
+        return client.send(self.message_bytes), 0
 
 
 class ParseURI(argparse.Action):  # pylint: disable=too-few-public-methods
@@ -108,12 +129,70 @@ def main():
             threads.append(enforcer)
             enforcer.start()
 
+        client = threads[0]._get_client()
+        print(os.linesep.join([
+            '',
+            '### Summary',
+            '',
+            '    * Thread num: {}',
+            '    * Protocol: {}',
+            '    * Address: {}',
+            '    * IP: {}',
+            '    * Port: {}',
+        ]).format(
+            args.thread_mum,
+            client.get_scheme(),
+            client.address,
+            client.ip,
+            client.port,
+        ))
+
         while True:
             time.sleep(0.2)
     except KeyboardInterrupt:
         for thread in threads:
             thread.stop = True
             thread.join()
+
+        if not threads or not all(map(lambda thread: thread.stats is not None, threads)):
+            return
+        output_template = os.linesep.join([
+            '',
+            '### Statistics',
+            '',
+            '* Requests',
+            '    * Num: {}',
+            '    * Speed: {:.2f} req/s',
+            '    * Failed ratio: {:.2f} %',
+            '    * Succeeded ratio: {:.2f} %',
+            '* Bandwith',
+            '    * Upload: {:.2f} KB/s',
+            '    * Download: {:.2f} KB/s',
+        ])
+        output = output_template.format(
+            sum([
+                thread.stats.succeeded_request_num + thread.stats.failed_request_num
+                for thread in threads
+            ]),
+            (sum([ thread.stats.succeeded_request_num + thread.stats.failed_request_num for thread in threads ]) /
+                sum([thread.stats.time_interval for thread in threads])),
+            sum([
+                thread.stats.failed_request_num / (thread.stats.succeeded_request_num + thread.stats.failed_request_num)
+                for thread in threads
+                if thread.stats.succeeded_request_num or thread.stats.failed_request_num
+            ]) * 100.0 / len(threads),
+            sum([
+                thread.stats.succeeded_request_num /
+                (thread.stats.succeeded_request_num + thread.stats.failed_request_num)
+                for thread in threads
+                if thread.stats.succeeded_request_num or thread.stats.failed_request_num
+            ]) * 100.0 / len(threads),
+            (sum([thread.stats.sent_byte_count for thread in threads]) /
+                sum([thread.stats.time_interval for thread in threads])) / 1000.0,
+            (sum([thread.stats.received_byte_count for thread in threads]) /
+                sum([thread.stats.time_interval for thread in threads])) / 1000.0,
+        )
+        print(output)
 
 
 if __name__ == '__main__':
