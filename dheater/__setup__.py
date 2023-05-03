23b0eb014e95e9273fc6db88b27177b80557f112
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2023-05-03 22:58:24 +0200

    Bump version number to 0.4.0

--- dheater/__setup__.py
+++ dheater/__setup__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 __title__ = 'DHEater'
-__version__ = '0.3.2'
+__version__ = '0.4.0'
 __description__ = 'D(HE)ater is an attacking tool heating the CPU by enforcing DHE KEX in case of TLS and SSH'
 __author__ = 'BalaSys IT Security'
 __maintainer__ = 'Szilárd Pfeiffer'
